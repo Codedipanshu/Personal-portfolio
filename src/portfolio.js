96c37f32cc0e9602cc1343f772cb1ab121773361
Dipanshu Agrawal <codedipanshu@gmail.com>
2023-11-29 20:25:36 +0530

    Initial portfolio

---
+++ src/portfolio.js
@@ -0,0 +1,395 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
+
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "Dipanshu Agrawal",
+  title: "Hi, I'm Dipanshu",
+  subTitle: emoji(
+    "A passionate Full Stack Software Developer 🚀 having experties in building Web applications with JavaScript / Reactjs / Nodejs / Express and some other cool libraries and frameworks."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/Codedipanshu",
+  linkedin: "https://www.linkedin.com/in/dipanshu-agrawal-99b76321b/",
+  gmail: "codedipanshu@gmail.com",
+  // Instagram, Twitter and Kaggle are also supported in the links!
+  // To customize icons and social links, tweak src/components/SocialMedia
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji("⚡ Able to develop functional backend for your web applications"),
+    emoji("⚡ Productive Web Applications in normal and MERN Stacks"),
+    emoji("⚡ Having some knowledge Android development ")
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "MongoDB",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "Java",
+      fontAwesomeClassname: "fab fa-java"
+    },
+    {
+      skillName: "Android Studio",
+      fontAwesomeClassname: "fab fa-android"
+    },
+
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "IGNOU",
+      logo: require("./assets/images/ignou.png"),
+      subHeader: "Bachlor of Computer Applications",
+      duration: "July 2021 - June 2024",
+      desc: "Took courses about Software Engineering, Data Structures, Web Development, Web Security, Operating Systems, ...",
+      descBullets: [
+        "Studying BCA to gain valuable knowledge about Computer Science."
+      ]
+    },
+    {
+      schoolName: "Rajasthan University",
+      logo: require("./assets/images/uniraj.png"),
+      subHeader: "Bachlor of Science General",
+      duration: "July 2018 - June 2021"
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Backend", //Insert stack or technology you have experience in
+      progressPercentage: "80%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "70%"
+    },
+    {
+      Stack: "Frontend/Design",
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: false, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Software Engineer",
+      company: "Facebook",
+      companylogo: require("./assets/images/facebookLogo.png"),
+      date: "June 2018 – Present",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      descBullets: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      ]
+    },
+    {
+      role: "Front-End Developer",
+      company: "Quora",
+      companylogo: require("./assets/images/quoraLogo.png"),
+      date: "May 2017 – May 2018",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      role: "Software Engineer Intern",
+      company: "Airbnb",
+      companylogo: require("./assets/images/airbnbLogo.png"),
+      date: "Jan 2015 – Sep 2015",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Projects",
+  subtitle:
+    "SOME OF COOL STUFF THAT I CREATED THAT SHOWCASE MY SKILLS AND ABILITY IN WEB DEV WORLD",
+  projects: [
+    {
+      image: require("./assets/images/Proshop.png"),
+      projectName: "ProShop",
+      projectDesc:
+        "ProShop, a fully functional e-Commerce Platform with PayPal integration & Admin Panel",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://proshop-ftr5.onrender.com/"
+        },
+        {
+          name: "GitHub Repo",
+          url: "https://github.com/Codedipanshu/proshop/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/Todo-list.png"),
+      projectName: "ToDo List",
+      projectDesc:
+        "Todo List web application having insert and delete functionality with two different lists",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://todo-list-f405.onrender.com/"
+        },
+        {
+          name: "GitHub Repo",
+          url: "https://github.com/Codedipanshu/ToDo-List"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "MERN From Scratch 2023",
+      subtitle: "Completed Certifcation from Udemy for MERN Stack Development",
+      image: require("./assets/images/Udemy.png"),
+      imageAlt: "Udemy Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://udemy-certificate.s3.amazonaws.com/pdf/UC-1222e239-cdb2-42f1-90dc-f0a188b5b9a2.pdf"
+        }
+      ]
+    },
+    {
+      title: "The Complete 2023 Web Development Bootcamp",
+      subtitle:
+        "Completed Certifcation from Udemy for Web Development Bootcamp",
+      image: require("./assets/images/Udemy.png"),
+      imageAlt: "Udemy Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://udemy-certificate.s3.amazonaws.com/pdf/UC-a7705710-c3d9-481e-b9db-0ec99f7c9fca.pdf"
+        }
+      ]
+    },
+    {
+      title: "CS50's Introduction to Computer Science",
+      subtitle: "Completed Certifcation from HarvardX for CS50x Programming",
+      image: require("./assets/images/harvardLogo.png"),
+      imageAlt: "Harward Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://certificates.cs50.io/90bfb137-de12-4635-9af0-82de3b25204b.pdf?size=letter"
+        }
+      ]
+    },
+    {
+      title: "Android App Development",
+      subtitle: "Completed Certifcation from Internshala for Android App Development",
+      image: require("./assets/images/internshala.webp"),
+      imageAlt: "Internshala Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://trainings.internshala.com/s/v/2942762/bc074d20"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
+  blogs: [
+    {
+      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/"
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just say hi.",
+  number: "+91-9057546379",
+  email_address: "codedipanshu@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "twitter", //Replace "twitter" with your twitter username without @
+  display: false // Set true to display this section, defaults to false
+};
+
+const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  splashScreen,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+  isHireable
+};
